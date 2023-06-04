d0c8a8c0ff2eada71fab670a8b0c375d025282b6
khekrn <kishorenayark@gmail.com>
2023-06-04 09:27:59 +0530

    Implementing arrays and updating comments in cache

--- ultimate-go/lesson2/caching/caching.go
+++ ultimate-go/lesson2/caching/caching.go
@@ -55,6 +55,26 @@ func init() {
 }
 
 // LinkedListTraverse traverses the linked list linearly.
+// The linked list is twice as slow as the row traversal mainly because
+// there are cache line misses but fewer TLB (Translation Lookaside Buffer)
+// misses. A bulk of the nodes connected in the list exist inside the same
+// OS pages.
+
+// Each running program is given a full memory map of virtual memory by the
+// OS and that running program thinks they have all of the physical memory
+// on the machine. However, physical memory needs to be shared with all the
+// running programs. The operating system shares physical memory by breaking
+// the physical memory into pages and mapping pages to virtual memory for
+// any given running program. Each OS can decide the size of a page, but 4k,
+// 8k, 16k are reasonable and common sizes.
+
+// The TLB is a small cache inside the processor that helps to reduce latency
+// on translating a virtual address to a physical address within the scope of
+// an OS page and offset inside the page. A miss against the TLB cache can cause
+// large latencies because now the hardware has to wait for the OS to scan its
+// page table to locate the right page for the virtual address in question.
+// If the program is running on a virtual machine (like in the cloud) then the
+// virtual machine paging table needs to be scanned first.
 func LinkedListTraversal() int {
 	var ctr int
 
@@ -71,6 +91,10 @@ func LinkedListTraversal() int {
 }
 
 // ColumnTraverse traverses the matrix linearly down each column.
+// Column Traverse is the worst by an order of magnitude because this access
+// pattern crosses over OS page boundaries on each memory access. This
+// causes no predictability for cache line prefetching and becomes essentially
+// random access memory.
 func ColumnTraverse() int {
 	var ctr int
 
@@ -86,6 +110,10 @@ func ColumnTraverse() int {
 }
 
 // RowTraverse traverses the matrix linearly down each row.
+// Row traverse will have the best performance because it walks through
+// memory, cache line by connected cache line, which creates a predictable
+// access pattern. Cache lines can be prefetched and copied into the L1 or
+// L2 cache before the data is needed.
 func RowTraverse() int {
 	var ctr int
 
