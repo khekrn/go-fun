ab747aba1b1b9e9e6a1ce405b358c0b5a19ac624
khekrn <kishorenayark@gmail.com>
2024-10-08 09:44:05 +0530

    Adding required comments

--- concurrency-go/hello-world/lesson2/cpu.go
+++ concurrency-go/hello-world/lesson2/cpu.go
@@ -6,7 +6,9 @@ import (
 )
 
 func main() {
+	// Total CPU core
 	fmt.Println("Total no of CPU = ", runtime.NumCPU())
+	// Used to set how many cores we want to use for go process
 	fmt.Println("Go Max Process = ", runtime.GOMAXPROCS(0))
 	contextSwitch()
 }
